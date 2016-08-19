11b7ea3af9396e756136b4baa3755e6c20646756
resoliwan <resoliwan@naver.com>
2016-08-19 18:38:41 +0900

    sync

---
+++ scratch/normal.py
@@ -0,0 +1,56 @@
+import math
+
+def normal_pdf(x, mu=0, sigma=1):
+    sqrt_two_pi = math.sqrt(2 * math.pi)
+    return (math.exp(-(x-mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma))
+
+
+def normal_cdf(x, mu=0, sigma=1):
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+    """이진 검색을 사용해서 역함수를 근사"""
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p)
+    low_z, low_p = -10.0, 0
+    hi_z, hi_p = 10.0, 1
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2
+        mid_p = normal_cdf(mid_z)
+        if mid_p < p:
+            low_z, low_p = mid_z, mid_p
+        elif mid_p > p:
+            hi_z, hi_p = mid_z, mid_p
+        else:
+            break
+
+    return mid_z
+
+# 누적 분포함수는 확률변수가 특정 값보다 작을 확률을 나타낸다
+normal_probability_below = normal_cdf
+
+# 만약 확율변수가 특정 값보다 작지 않다면, 특정 값보다 크다는 것을 의미한다
+def normal_probability_above(lo, mu=0, sigma=1):
+    return 1 - normal_cdf(lo, mu, sigma)
+
+def normal_probability_between(lo, hi, mu=0, sigma=1):
+    return normal_cdf(hi, mu, sigma) - normal_cdf(lo, mu, sigma)
+
+def normal_probability_outside(lo, hi, mu=0, sigma=1):
+    return 1 - normal_probability_between(lo, hi, mu, sigma)
+
+def normal_upper_bound(probability, mu=0, sigma=1):
+    """ P(Z <= z) = probability 인 z 값을 반환"""
+    return inverse_normal_cdf(probability, mu, sigma)
+
+def normal_lower_bound(probability, mu=0, sigma=1):
+    """ P(Z >= z) = probability 인 z 값을 반환"""
+    return inverse_normal_cdf(1 - probability, mu, sigma)
+
+def normal_two_sided_bounds(probability, mu=0, sigma=1):
+    tail_probability = (1 - probability) / 2
+
+    upper_bound = normal_lower_bound(tail_probability, mu, sigma)
+    lower_bound = normal_upper_bound(tail_probability, mu, sigma)
+
+    return lower_bound, upper_bound
